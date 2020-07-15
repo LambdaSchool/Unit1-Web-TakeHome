a685d47e8575aa0fcca4eeea164414751cf9915c
Daniel Frehner <dfrehner1@gmail.com>
2020-07-15 16:37:05 -0600

    Updated instructions

--- Unit1-Instructor-TakeHome/javaScript/classes.js
+++ Unit1-Instructor-TakeHome/javaScript/classes.js
@@ -1,6 +1,5 @@
-// 1. Copy and paste your prototype in here and refactor into class syntax.
+// 1. Copy and paste the CuboidMaker constructor and prototype (from prototypes.js) in here and refactor into ES6 class syntax.
 
 // Test your volume and surfaceArea methods by uncommenting the logs below:
 // console.log(cuboid.volume()); // 100
 // console.log(cuboid.surfaceArea()); // 130
-
